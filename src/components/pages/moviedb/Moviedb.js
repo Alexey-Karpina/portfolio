986e8a5d602ff0d3fdf45570f0c908e39007830a
user <C:\Users\User\AppData\Roaming\The Bat!>
2021-02-01 17:20:38 +0200

    Added movie Header and simple slider for hot movies

--- src/components/pages/moviedb/Moviedb.js
+++ src/components/pages/moviedb/Moviedb.js
@@ -1,13 +1,19 @@
 import React from "react";
-import MovieService from './services/movie-service';
-
+import MovieHeader from "./components/movie-header/movieHeader";
+import MovieService from "./services/movie-service";
+import RandomMovie from "./components/random-movie/randomMovie";
 
 const Moviedb = () => {
   const db = new MovieService();
   db.getTrendingMovies().then((res) => console.log(res.results));
   db.getMovieDetails(25).then((res) => console.log(res));
-  const imageUrl = db.getMoviePoster('/4rCzaZy5Qkvxh5xaVpHriXSLTgC.jpg');
-  return <img src={imageUrl}></img>;
+  const imageUrl = db.getMoviePoster("/4rCzaZy5Qkvxh5xaVpHriXSLTgC.jpg");
+  return (
+    <div className="wrapper">
+      <MovieHeader />
+      <RandomMovie />
+    </div>
+  );
 };
 
 export default Moviedb;
--- src/components/pages/moviedb/components/random-movie/randomMovie.js
+++ src/components/pages/moviedb/components/random-movie/randomMovie.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import './randomMovie.css';
+import React from "react";
+import "./randomMovie.css";
+import sliderBG from "../../images/slider-bg.png";
+import Slider from "react-slick";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnHover: true,
+  beforeChange: function (currentSlide, nextSlide) {
+    console.log("before change", currentSlide, nextSlide);
+  },
+  afterChange: function (currentSlide) {
+    console.log("after change", currentSlide);
+  },
+};
 
 const RandomMovie = () => {
-    return(<></>);
+  return (
+    <section className="movie__slider">
+      <div className="slider-window">
+        <Slider {...settings}>
+          <div className="slider-window">
+            <img src={sliderBG} />
+          </div>
+          <div className="slider-window">
+            <img src={sliderBG} />
+          </div>
+          <div className="slider-window">
+            <img src={sliderBG} />
+          </div>
+        </Slider>
+      </div>
+    </section>
+  );
 };
 
 export default RandomMovie;
